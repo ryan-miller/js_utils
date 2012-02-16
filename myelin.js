f0e5e3e29642577ba15d5851eb8fd45e74d652b6
Ryan Miller <miller2300@comcast.net>
2012-02-16 17:08:30 -0500

    added some error handling

--- myelin.js
+++ myelin.js
@@ -1,29 +1,62 @@
 var randomIntegerBetween = function(f,t) {
-      var r =  Math.floor(Math.random() * (t - f + 1)) + f; 
-      console.log("Random number between " + f + " and " + t + " is " + r);
-      return r;
+      // make sure f and t are numbers and f and t are numbers
+      try {
+         if (f <= t) {
+            var r =  Math.floor(Math.random() * (t - f + 1)) + f; 
+            console.log("Random number between " + f + " and " + t + " is " + r);
+            return r;
+         } else {
+            throw new Error("Cannot generate random integer. From("+f+") is greater than to("+t+")");
+         }
+      }
+      catch(ex) {
+         console.log(ex)
+         return false;
+      }
+      
 };
 
 var randomFloatBetween = function(f,t) {
-   var r = Math.random() * (t-f) + f;
-   console.log("Random number between " + f + " and " + t + " is " + r);
-   return r;
+   // make sure f and t are numbers
+   try {
+      if ((f <= t) && !isNaN(f) && !isNaN(t)) {
+         var r = Math.random() * (t-f) + f;
+         console.log("Random number between " + f + " and " + t + " is " + r);
+         return r;
+      } else {
+         throw new Error("Cannot generate random float");
+      }
+   } 
+   catch(ex) {
+      console.log(ex);
+      return false;
+   }
 };
 
+// do we need both row and columns?  can rows be inferred from columns?
 var to2D = function(s, r, c) {
-   // todo: make sure array is Array
-   var a = [],
-      r = [],
-      l = s.length,
-      i = 0;
-   
-   for (i; i<l; i++) {
-      r.push(s[i]);
-     
-      if((i+1) % c === 0 && i !== 0) {
-         a.push(r);
-         r=[];
+   // todo: make sure s is Array
+   try {
+      if (typeof s === "object"  && s !== null) { 
+         var a = [],
+            r = [],
+            l = s.length,
+            i = 0;
+
+         for (i; i<l; i++) {
+            r.push(s[i]);
+            if((i+1) % c === 0 && i !== 0) {
+               a.push(r);
+               r = [];
+            }
+         }
+         return a;
+      } else {
+         throw new Error("source("+s+") must be an array not "+typeof s);
       }
    }
-   return a;
+   catch(ex) {
+      console.log(ex);
+      return false;
+   }
 };
--- myelin_tests.js
+++ myelin_tests.js
@@ -20,6 +20,10 @@ test('Random numbers', function() {
    ok(isIntBetween(0,1), 'positive integers');
    ok(isIntBetween(-1,0), 'negative integers');
    ok(isIntBetween(-100, 100), 'span zero');
+   ok(isIntBetween(null, 1),'check from null');
+   ok(isIntBetween(0, null),'check to null');
+   ok(!isIntBetween(5, 1),'from is greater than to');
+   ok(!isIntBetween("Five", 0), 'numbers only');
    
    // floats
    ok(isFloatBetween(1.9,10.9), 'positive floats');
@@ -27,11 +31,17 @@ test('Random numbers', function() {
    ok(isFloatBetween(0.0,1.9), 'positive floats');
    ok(isFloatBetween(-1.9,0.0), 'negative floats');
    ok(isFloatBetween(-100.4, 100.7), 'span zero');
+   ok(isFloatBetween(null, 1),'check from null');
+   ok(isFloatBetween(0, null),'check to null');
+   ok(!isFloatBetween(5, 1),'from is greater than to');
+   ok(!isFloatBetween("Five", 0), 'numbers only');
 });
 
 test('Array functions', function() {
    // 1d array to 2d array
-   ok(isCorrectArray([0,0,0,0], 2, 2, [[0,0],[0,0]]));
+   ok(isCorrectArray([0,0,0,0], 2, 2, [[0,0],[0,0]]),'basic array');
+   ok(!isCorrectArray(null, 2, 2, null), 'handles null source arrays')
+   // user provides in correct dimensions
    
    // 2d array to 1d array
    
